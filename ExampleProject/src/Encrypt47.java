9c57d95a5abbdd283c0b2d5618d406c26e4b5de9
colemoebius <colemoebius1@gmail.com>
2016-05-12 13:23:45 -0700

    started rot47

---
+++ ExampleProject/src/Encrypt47.java
@@ -0,0 +1,94 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Encrypt47 implements Interface47{
+	Scanner reader;
+	File file;
+	String plaintext="";
+	String ciphertext="";
+	public Encrypt47(){
+		
+	}
+public void Read(){
+			try {
+			reader = new Scanner(new File("C:\\Users\\moebiusc5097\\Desktop\\input.txt"));
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+		
+		while (reader.hasNext()){
+			  plaintext += reader.nextLine();
+		}
+	}
+ 	
+
+public char rotate47(char add47){
+	
+		if(add47 != ' '){
+		add47+=47;
+	
+		if(add47 > 126){
+			add47-=94;
+		}
+		}
+	return add47;
+	
+}
+public String encrypt(){
+	for(int i = 0; i<plaintext.length();i++){
+		 
+		char temp = rotate47(plaintext.charAt(i));
+		ciphertext+= temp;
+	}
+	return ciphertext.toString();
+	
+}
+public void Write(){
+try {
+
+	
+
+	 file = new File("C:\\Users\\moebiusc5097\\Desktop\\encrypted.txt");
+
+	
+	if (!file.exists()) {
+		file.createNewFile();
+	}
+
+	FileWriter fw = new FileWriter(file.getAbsoluteFile());
+	BufferedWriter bw = new BufferedWriter(fw);
+	bw.write(ciphertext);
+	bw.close();
+
+	
+} catch (IOException e) {
+	e.printStackTrace();
+}
+}
+
+public String toString(){
+	return ciphertext;
+}
+public static void main(String[] args) {
+	Encrypt47 encrypted = new Encrypt47();
+	Decrypt47 decrypted = new Decrypt47();
+	encrypted.Read();
+	encrypted.encrypt();
+	System.out.println(encrypted);
+	decrypted.Read();
+	decrypted.decrypt();
+	System.out.println(decrypted);
+	
+	
+		
+	
+}
+}
---
+++ ExampleProject/src/Interface47.java
@@ -0,0 +1,7 @@
+
+public interface Interface47 {
+	public void Read();
+	public char rotate47(char add47);
+	public void Write();
+	
+}
